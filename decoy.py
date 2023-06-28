0f91d3442246201215ab7979951e9a3a714c21c6
Ian George <ian@iangeorge.net>
2023-06-28 16:45:41 +0100

    Add a (naive but function) python oversampling function

--- decoy.py
+++ decoy.py
@@ -6,6 +6,21 @@ import pandas as pd
 import pyarrow as pa
 import random
 
+# global_con = duckdb.connect("test.duckdb")
+
+cached_column = []
+
+
+def cache_column(table_name, column_name):
+    global cached_column
+    con = duckdb.connect("test.duckdb")
+    con.execute(f"SELECT {column_name} FROM {table_name}")
+    cached_column = [val[0] for val in con.fetchall()]
+
+
+def get_from_cache():
+    return random.choice(cached_column)
+
 
 def get_faker_locale(locale: str) -> Callable[[str], Any]:
     fkr = Faker(locale)
@@ -25,10 +40,23 @@ def random_shuffle(x):
     shuffled = df.sample(frac=1, ignore_index=True)
     return pa.lib.Table.from_pandas(shuffled)
 
+
 def intratable_sample(x):
     df = pd.DataFrame(x.to_pandas())
     shuffled = df.sample(frac=1, replace=True, ignore_index=True)
     return pa.lib.Table.from_pandas(shuffled)
+
+
+def oversample(table_name, column_name):
+    # global_con.execute(
+    #     f"SELECT {column_name} FROM {table_name} ORDER BY RANDOM() LIMIT 1"
+    # )
+    # return global_con.fetchone()[0]
+
+    if not len(cached_column):
+        cache_column(table_name, column_name)
+
+    return get_from_cache()
 
 
 def register_en(con: duckdb.DuckDBPyConnection) -> None:
@@ -64,4 +92,12 @@ def register_en(con: duckdb.DuckDBPyConnection) -> None:
         parameters=ducktypes.VARCHAR,
         side_effects=True,
         type=duckdb.functional.PythonUDFType.ARROW,
+    )
+
+    con.create_function(
+        name="oversample",
+        function=oversample,
+        return_type=[ducktypes.VARCHAR, ducktypes.VARCHAR],
+        parameters=ducktypes.VARCHAR,
+        side_effects=True,
     )
