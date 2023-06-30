ea74376f2505407caef56063373a926c17ea3616
Ian George <ian@iangeorge.net>
2023-06-30 16:45:21 +0100

    Fix tests broken by settings refactor

--- tests/test_decoy.py
+++ tests/test_decoy.py
@@ -1,8 +1,9 @@
 import pytest
 
-from decoy.database import settings, column_cache, get_connection, cache_column
+from decoy.database import column_cache, get_connection, cache_column
+from decoy.settings import settings
 
-settings["database_file"] = 'test.duckdb'
+settings.database_file = "test.duckdb"
 
 
 def test_func(connection):
@@ -15,14 +16,17 @@ def connection(request):
 
     def teardown():
         print("teardown")
+
     request.addfinalizer(teardown)
 
     return get_connection()
 
 
 def test_cache_column(connection):
-    connection.execute('''
+    connection.execute(
+        """
     CREATE TABLE IF NOT EXISTS test_cache_column AS SELECT range from range(10)
-    ''')
-    cache_column('test_cache_column', 'range')
-    assert len(column_cache['test_cache_column.range']) == 10
+    """
+    )
+    cache_column("test_cache_column", "range")
+    assert len(column_cache["test_cache_column.range"]) == 10
