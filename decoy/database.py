23bb41aaa0695aafba1b48d43a1bd35ae03151c3
Ian George <ian@iangeorge.net>
2023-07-05 12:24:37 +0100

    Refactor database stuff out to scalar, arrow function specific modules

--- decoy/database.py
+++ decoy/database.py
@@ -1,15 +1,13 @@
 import duckdb
 from duckdb import typing as ducktypes
-from faker import Faker
-from mimesis import Generic, Locale
-import pandas as pd
-import pyarrow as pa
-import random
-from typing import Callable, Any
 
 from decoy.settings import settings
-
-column_cache = {}
+from decoy.udf_scalar import (
+    get_faker_locale,
+    get_mimesis_locale,
+    custom_choice_generator,
+)
+from decoy.udf_arrow import random_shuffle, intratable_sample, oversample
 
 
 def get_connection(register_funcs=True) -> duckdb.DuckDBPyConnection:
@@ -17,62 +15,6 @@ def get_connection(register_funcs=True) -> duckdb.DuckDBPyConnection:
     if register_funcs:
         register_udfs(con)
     return con
-
-
-def cache_column(table_name: str, column_name: str) -> None:
-    con = get_connection(False)
-    con.execute(f"SELECT {column_name} FROM {table_name}")
-    column_cache[f"{table_name}.{column_name}"] = [val[0] for val in con.fetchall()]
-
-
-def get_faker_locale(locale: str) -> Callable[[str], Any]:
-    fkr = Faker(locale)
-
-    def dispatch(fname: str):
-        return getattr(fkr, fname)()
-
-    return dispatch
-
-
-def get_mimesis_locale(locale: str) -> Callable[[str], Any]:
-    loc = getattr(Locale, locale)
-    mim = Generic(loc)
-
-    def dispatch(fname: str):
-        fnames = fname.split(".")
-        generator = mim
-        for att in fnames:
-            generator = getattr(generator, att)
-
-        return generator()
-
-    return dispatch
-
-
-def custom_choice_generator() -> ducktypes.VARCHAR:
-    return random.choice(["Fake 1", "Fake 2", "Fake 3"])
-
-
-def random_shuffle(x: list[list[Any]]) -> pa.Table:
-    df = pd.DataFrame(x.to_pandas())
-    shuffled = df.sample(frac=1, ignore_index=True)
-    return pa.lib.Table.from_pandas(shuffled)
-
-
-def intratable_sample(x: list[list[Any]]) -> pa.Table:
-    df = pd.DataFrame(x.to_pandas())
-    shuffled = df.sample(frac=1, replace=True, ignore_index=True)
-    return pa.lib.Table.from_pandas(shuffled)
-
-
-def oversample(table_name: str, column_name: str) -> str:
-    """
-    #TODO: explain why we need to cache the column again!
-    """
-    cache_column(table_name, column_name)
-
-    col_ref = f"{table_name}.{column_name}"
-    return random.choice(column_cache[col_ref])
 
 
 def register_udfs(con: duckdb.DuckDBPyConnection) -> None:
---
+++ decoy/udf_scalar.py
@@ -0,0 +1,33 @@
+from duckdb import typing as ducktypes
+from faker import Faker
+from mimesis import Generic, Locale
+import random
+from typing import Callable, Any
+
+
+def get_faker_locale(locale: str) -> Callable[[str], Any]:
+    fkr = Faker(locale)
+
+    def dispatch(fname: str):
+        return getattr(fkr, fname)()
+
+    return dispatch
+
+
+def get_mimesis_locale(locale: str) -> Callable[[str], Any]:
+    loc = getattr(Locale, locale)
+    mim = Generic(loc)
+
+    def dispatch(fname: str):
+        fnames = fname.split(".")
+        generator = mim
+        for att in fnames:
+            generator = getattr(generator, att)
+
+        return generator()
+
+    return dispatch
+
+
+def custom_choice_generator() -> ducktypes.VARCHAR:
+    return random.choice(["Fake 1", "Fake 2", "Fake 3"])
